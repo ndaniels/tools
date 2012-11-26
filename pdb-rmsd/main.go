950aa4eafb2c941f13359f1412f2beeca9aa1acb
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-11-25 19:32:40 -0500

    Reorganizing secondary tools.
    Only the "main" commands of the bcbgo package should be in the 'cmd'
    directory. All other tools (mostly stored as "examples" before this)
    should be in the 'tools' directory. Tools are not required to use bcbgo,
    but are typically useful for intermediate tasks and are necessary for
    running experiments.
    
    Also, I've fixed the output of HHM files (including the MSA part) so
    that they can be read by HHfrag's parser. Ticky tack!

---
+++ pdb-rmsd/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+
+	"github.com/BurntSushi/bcbgo/io/pdb"
+	"github.com/BurntSushi/bcbgo/rmsd"
+)
+
+func main() {
+	if flag.NArg() != 8 {
+		usage()
+	}
+
+	// Nice aliases.
+	pdbf1, chain1, s1, e1 := flag.Arg(0), flag.Arg(1), flag.Arg(2), flag.Arg(3)
+	pdbf2, chain2, s2, e2 := flag.Arg(4), flag.Arg(5), flag.Arg(6), flag.Arg(7)
+
+	// Build pdb.Entry values. If anything goes wrong, quit!
+	entry1, err := pdb.New(pdbf1)
+	if err != nil {
+		fatalf("%s", err)
+	}
+	entry2, err := pdb.New(pdbf2)
+	if err != nil {
+		fatalf("%s", err)
+	}
+
+	// Now make sure the slice numbers are valid integers.
+	s1n, e1n, s2n, e2n := parseInt(s1), parseInt(e1), parseInt(s2), parseInt(e2)
+
+	r, err := rmsd.PDB(entry1, chain1[0], s1n, e1n, entry2, chain2[0], s2n, e2n)
+	if err != nil {
+		fatalf("%s", err)
+	}
+	fmt.Println(r)
+}
+
+func parseInt(numStr string) int {
+	num, err := strconv.ParseInt(numStr, 10, 32)
+	if err != nil {
+		fatalf("Could not parse '%s' as an integer.", numStr)
+	}
+	return int(num)
+}
+
+func fatalf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+	fmt.Fprintln(os.Stderr, "")
+	os.Exit(1)
+}
+
+func init() {
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"Usage: %s pdb-file chain-id start stop pdb-file chain-id start stop\n",
+		path.Base(os.Args[0]))
+	flag.PrintDefaults()
+	fmt.Fprintf(os.Stderr,
+		"\nex. './%s sample1.pdb A 30 40 sample1.pdb A 40 50'\n",
+		path.Base(os.Args[0]))
+	os.Exit(1)
+}
---
+++ pdb2fasta/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/BurntSushi/bcbgo/io/fasta"
+	"github.com/BurntSushi/bcbgo/io/pdb"
+	"github.com/BurntSushi/bcbgo/seq"
+)
+
+var (
+	flagChain          = ""
+	flagSeparateChains = false
+	flagSeqRes         = false
+	flagSplit          = ""
+)
+
+func main() {
+	if flag.NArg() < 1 || flag.NArg() > 2 {
+		usage()
+	}
+
+	pdbEntry, err := pdb.New(flag.Arg(0))
+	if err != nil {
+		fatalf("Could not read PDB file '%s': %s", flag.Arg(0), err)
+	}
+
+	var fasOut io.Writer
+	if flag.NArg() == 1 {
+		fasOut = os.Stdout
+	} else {
+		if len(flagSplit) > 0 {
+			fatalf("The '--split' option is incompatible with a single " +
+				"output file.")
+		}
+		fasOut, err = os.Create(flag.Arg(1))
+		if err != nil {
+			fatalf("Could not create FASTA file '%s': %s", flag.Arg(1), err)
+		}
+	}
+
+	fasEntries := make([]seq.Sequence, 0, 5)
+	if !flagSeparateChains {
+		var fasEntry seq.Sequence
+		if len(pdbEntry.Chains) == 1 {
+			fasEntry.Name = chainHeader(pdbEntry.OneChain())
+		} else {
+			fasEntry.Name = fmt.Sprintf("%s", pdbEntry.IdCode)
+		}
+
+		seq := make([]seq.Residue, 0, 100)
+		for _, chain := range pdbEntry.Chains {
+			if isChainUsable(chain) {
+				seq = append(seq, getChainSequence(chain)...)
+			}
+		}
+		fasEntry.Residues = seq
+
+		if len(fasEntry.Residues) == 0 {
+			fatalf("Could not find any amino acids.")
+		}
+		fasEntries = append(fasEntries, fasEntry)
+	} else {
+		for _, chain := range pdbEntry.Chains {
+			if !isChainUsable(chain) {
+				continue
+			}
+
+			fasEntry := seq.Sequence{
+				Name:     chainHeader(chain),
+				Residues: getChainSequence(chain),
+			}
+			fasEntries = append(fasEntries, fasEntry)
+		}
+	}
+
+	if len(fasEntries) == 0 {
+		fatalf("Could not find any chains with amino acids.")
+	}
+	if len(flagSplit) == 0 {
+		if err := fasta.NewWriter(fasOut).WriteAll(fasEntries); err != nil {
+			fatalf("Could not write FASTA: %s", err)
+		}
+	} else {
+		for _, entry := range fasEntries {
+			fp := path.Join(flagSplit, fmt.Sprintf("%s.fasta", entry.Name))
+			out, err := os.Create(fp)
+			if err != nil {
+				fatalf("Could not create FASTA file: %s", err)
+			}
+
+			w := fasta.NewWriter(out)
+			if err := w.Write(entry); err != nil {
+				fatalf("Could not write to FASTA file: %s", err)
+			}
+			if err := w.Flush(); err != nil {
+				fatalf("Could not write to FASTA file: %s", err)
+			}
+		}
+	}
+}
+
+func chainHeader(chain *pdb.Chain) string {
+	return fmt.Sprintf("%s%c", strings.ToLower(chain.Entry.IdCode), chain.Ident)
+}
+
+func getChainSequence(chain *pdb.Chain) []seq.Residue {
+	if flagSeqRes {
+		return chain.Sequence
+	}
+	return chain.CaSequence
+}
+
+func isChainUsable(chain *pdb.Chain) bool {
+	if len(flagChain) == 0 {
+		return true
+	}
+	for i := 0; i < len(flagChain); i++ {
+		if chain.Ident == flagChain[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func fatalf(format string, v ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, v...)
+	fmt.Fprintln(os.Stderr, "")
+	os.Exit(1)
+}
+
+func init() {
+	flag.BoolVar(&flagSeparateChains, "separate-chains", flagSeparateChains,
+		"When set, each chain will get its own FASTA entry.")
+	flag.StringVar(&flagChain, "chain", flagChain,
+		"This may be set to one or more chain identifiers. Only amino acids "+
+			"belonging to a chain specified will be included.")
+	flag.BoolVar(&flagSeqRes, "seqres", flagSeqRes,
+		"When set, sequences will be read from the SEQRES records. Otherwise, "+
+			"sequences are read from residues in Ca ATOM records.")
+	flag.StringVar(&flagSplit, "split", flagSplit,
+		"When set, each FASTA entry produced will be written to a file in the "+
+			"specified directory with the PDB id code and chain identifier as "+
+			"the name.")
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"Usage: %s pdb2fasta [flags] in-pdb-file [out-fasta-file]\n",
+		path.Base(os.Args[0]))
+	flag.PrintDefaults()
+	os.Exit(1)
+}
