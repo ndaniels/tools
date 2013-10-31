16f30968c755fa236d18881cfbb30860b60070b9
Andrew Gallant <jamslam@gmail.com>
2013-10-31 00:45:12 -0400

    Small utility to split FASTA files into a file for each sequence.

---
+++ fasta-split/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"os"
+	path "path/filepath"
+	"strings"
+
+	"github.com/TuftsBCB/io/fasta"
+	"github.com/TuftsBCB/tools/util"
+)
+
+func init() {
+	util.FlagParse("fasta-file out-dir",
+		"Split a single FASTA file into a set of files for each sequence.")
+	util.AssertNArg(2)
+}
+
+func main() {
+	rfasta := util.OpenFasta(util.Arg(0))
+	dir := util.Arg(1)
+	util.Assert(os.MkdirAll(dir, 0777))
+
+	fr := fasta.NewReader(rfasta)
+	for {
+		s, err := fr.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			util.Assert(err)
+		}
+
+		s.Name = strings.Fields(s.Name)[0]
+		fw := util.CreateFile(path.Join(dir, s.Name+".fasta"))
+		w := fasta.NewWriter(fw)
+		util.Assert(w.Write(s))
+		util.Assert(w.Flush())
+		util.Assert(fw.Close())
+	}
+}
