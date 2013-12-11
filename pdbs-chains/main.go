2c8535807a41000bcc7002e46f0bb3ea5b8175f0
Andrew Gallant <jamslam@gmail.com>
2013-12-10 22:13:07 -0500

    A new tool to output a list of PDB ids from a PDB Select file.

---
+++ pdbs-chains/main.go
@@ -0,0 +1,43 @@
+// Command pdbs-chains reads a file in the PDB Select format and outputs the
+// list of PDB identifiers in the set.
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/TuftsBCB/io/pdb/slct"
+	"github.com/TuftsBCB/tools/util"
+)
+
+var (
+	flagPaths = false
+)
+
+func init() {
+	flag.BoolVar(&flagPaths, "paths", flagPaths,
+		"When set, the full path of each PDB chain identifier will be\n"+
+			"displayed, based on the value of the PDB_PATH environment\n"+
+			"variable.")
+
+	util.FlagParse("pdb-select-file",
+		"Given a file in the PDB Select format, output a list of PDB chain "+
+			"identifiers (one per line).")
+	util.AssertNArg(1)
+}
+
+func main() {
+	pdbs := util.OpenFile(flag.Arg(0))
+	defer pdbs.Close()
+
+	entries, err := slct.NewReader(pdbs).ReadAll()
+	util.Assert(err)
+
+	for _, entry := range entries {
+		if flagPaths {
+			fmt.Println(util.PDBPath(entry.ChainID))
+		} else {
+			fmt.Println(entry.ChainID)
+		}
+	}
+}
